56f73df820039dee7d97988c44e14f0a790b0cb8
Grigorii Chaikin <gnchaykin@mail.ru>
2025-01-18 19:18:12 +0700

    del __pycache__

--- src/category.py
+++ src/category.py
@@ -5,7 +5,7 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products=None):
+    def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products if products else []
